9da4b82e88300ba737e29617f5af5f47650c2a26
zyjcode00 <jiez75552@gmail.com>
2026-08-29 01:49:14 +0800

    🎯 [Task-1] Auto snapshot

--- terminal_bench_adapter.py
+++ terminal_bench_adapter.py
@@ -241,7 +241,6 @@ class MiniClaudeCodeTerminalBenchAgent(BaseAgent):
             max_history=150,
             min_keep=8,
             memory_manager=memory_manager,
-            enable_git_automation=False,
         )
 
     @staticmethod
